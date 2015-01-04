8a3f6bf892f4e5598f8bd9e1997708af01ab7cba
Erik Carlsson <info@code301.com>
2015-01-04 20:46:44 +0100

    Init

---
+++ lib/Task/MapMetro/Maps.pm
@@ -0,0 +1,45 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Task::MapMetro::Maps;
+
+# VERSION
+
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::MapMetro::Maps - Short intro
+
+=head1 SYNOPSIS
+
+  use Task::MapMetro::Maps;
+
+=head1 DESCRIPTION
+
+Task::MapMetro::Maps is ...
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015 - Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
